a85b09b36f0b844460ca3ea7a7c28e592b4a5bf1
Ehsen Siraj <ehsensiraj@gmail.com>
2024-12-07 17:58:22 +0500

    chore: updated import doc fields

---
+++ importmanager/importmanager/doctype/importdoc/test_importdoc.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SpotLedger and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestImportDoc(FrappeTestCase):
+	pass
