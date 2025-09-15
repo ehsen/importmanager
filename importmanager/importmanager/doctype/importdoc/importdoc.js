8f256a9db686b421433608b510ffd7ecde9c7af3
Ehsen Siraj <ehsensiraj@gmail.com>
2025-09-15 17:26:44 +0500

    chore: added update import data button in importdoc

--- importmanager/importmanager/doctype/importdoc/importdoc.js
+++ importmanager/importmanager/doctype/importdoc/importdoc.js
@@ -1,5 +1,27 @@
 frappe.ui.form.on("ImportDoc", {
     refresh: function(frm) {
+        // Add Update Import Data button only for saved documents
+        if (!frm.is_new()) {
+            let updateBtn = frm.add_custom_button(__("Update Import Data"), function() {
+                updateBtn.prop('disabled', true);
+                frappe.dom.freeze(__("Updating import data..."));
+                frappe.call({
+                    method: "importmanager.import_utils.update_data_in_import_doc",
+                    args: {
+                        import_doc_name: frm.doc.name
+                    }
+                }).then(() => {
+                    frm.reload_doc();
+                    frappe.show_alert({
+                        message: __("Import data updated successfully"),
+                        indicator: 'green'
+                    });
+                }).always(() => {
+                    updateBtn.prop('disabled', false);
+                    frappe.dom.unfreeze();
+                });
+            }).addClass('btn-primary');
+        }
         // Add custom button if document is not locked
         if (frm.doc.status !== "Locked") {
             frm.add_custom_button(__("Finalize and Generate Allocation Ledger Entries"), function() {
